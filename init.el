579e86299457edeb954237aae2902d98869064bf
Stas <stas@stas-G31T-M7.(none)>
2012-11-20 14:16:39 +0600

    new extensions

--- init.el
+++ init.el
@@ -19,10 +19,11 @@
 (setq display-time-24hr-format t)
 (display-time)
 
+(setq inhibit-startup-message t) ;;не показывать сообщение при старте
+(fset 'yes-or-no-p 'y-or-n-p) ;;не заставляйте меня печать yes целиком
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-
 
 (setq scroll-conservatively 10000)
 (setq scroll-preserve-screen-position 't)
@@ -32,7 +33,9 @@
 (line-number-mode 1)
 (column-number-mode 1)
 
-
+;; Line numbering
+(setq linum-format "%4d")
+(global-linum-mode 1)
 
 ;; Mouse
 (global-set-key [mouse-3] 'imenu)
@@ -102,6 +105,33 @@
 (require 'highlight-sexp)
 (add-hook 'lisp-mode-hook 'highlight-sexp-mode)
 (add-hook 'emacs-lisp-mode-hook 'highlight-sexp-mode)
+(add-hook 'python-mode-hook 'highlight-sexp-mode)
+(add-hook 'c-mode-hook 'highlight-sexp-mode)
+
 
 ;;ipython
-(require 'ipython) (setq py-python-command-args '( "-colors" "Linux")) (defadvice py-execute-buffer (around python-keep-focus activate) "return focus to python code buffer" (save-excursion ad-do-it)) 
+(add-to-list 'load-path "~/.emacs.d/extensions/ipython.el")
+(require 'ipython) (setq py-python-command-args '( "--colors=Linux")) (defadvice py-execute-buffer (around python-keep-focus activate) "return focus to python code buffer" (save-excursion ad-do-it)) 
+
+; Autocomplete python
+(add-to-list 'load-path "/home/stas/.emacs.d/extensions/auto-complete")
+(require 'auto-complete-config) (global-auto-complete-mode t)
+(add-to-list 'ac-dictionary-directories "/home/stas/.emacs.d/extensions/auto-complete/ac-dict")
+(ac-config-default)
+
+; Look definition of function or value:
+(defun rope-goto-definition-save-place ()
+   """ save current place as 'save-place' bookmark and rope-goto-definition """
+   (interactive)
+   (bookmark-set "save-place" 1)
+   (rope-goto-definition)
+)
+ 
+(defun rope-return ()
+   """ save current place as 'save-place' bookmark and rope-goto-definition """
+   (interactive)
+   (bookmark-jump "save-place")
+)
+ 
+(global-set-key [(M return)] 'rope-goto-definition-save-place)
+(global-set-key [(M shift return)] 'rope-return)
